6491a73e5d544032fc4309bbe3c543340d4cda9a
cossito <93940003+cossito@users.noreply.github.com>
2022-05-16 15:07:56 -0300

    Update Questão 1.cs

--- Listas 1 a 4/Lista 2/Questão 1.cs
+++ Listas 1 a 4/Lista 2/Questão 1.cs
@@ -3,6 +3,6 @@ class Program {
   public static void Main() {
     Console.WriteLine("Digite seu primeiro nome:");
     string s = Console.ReadLine();
-    Console.WriteLine("Bem vindo ao C#, " + s);
+    Console.WriteLine("Bem-vindo ao C#, " + s);
   }
 }
