73c60d59458754b1a67492f04a5bf3b65e427cf3
cossito <93940003+cossito@users.noreply.github.com>
2022-07-27 20:18:03 -0300

    Adicionei a nova lista de revisão

--- Listas/Lista Revisão 5/Questão 15.cs
+++ Listas/Lista Revisão 5/Questão 15.cs
@@ -1,6 +1,6 @@
 using System;
 public class Program{
-  public static void Main(string[] args){
+  public static void Main(){
     int x = int.Parse(Console.ReadLine());
     int y = int.Parse(Console.ReadLine());
     Console.WriteLine(UltimoDia(x, y));
